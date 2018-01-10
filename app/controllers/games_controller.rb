13cbe7a01b7c691c40145f6485f3513d0d04beb4
Monika Williams <monika.m.williams@gmail.com>
2018-01-10 13:23:47 +0000

    automatically backed up by learn

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,21 @@
 class GamesController < ApplicationController
   # Add your GamesController code here
+  def index
+    @games = Game.all
+  end
 
+  def create 
+    @game = Game.new
+  end
+  def show
+  end
+  
+  def updated
+  end
+  private
+
+  def set_game
+    @game = Game.find(params[:id])
+  end
+    
 end
